b396f51b11c5257e5c90dc2192f7ca687d394c3e
Anton Shabalov <shabalovanton@gmail.com>
2023-01-24 19:54:22 +0200

    start task  3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,24 +6,37 @@ import { nanoid } from 'nanoid';
 export class App extends Component {
   
   state = {
-    contacts: [{id: "jNXoZeO0-bIoz3ObfEAG7", name: "Anpu"}, {id: "j4Is3cCT0J1Aj8rLIHcKE", name: "Givgik"}],
-    name: ''
+    contacts: [{
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}
+    }],
+    name: '',
+    number: '',
+    filter: ''
   }
 
   recordName = (evnt) => {
     this.setState({name: evnt.target.value})
   }
   
+  recordNumber = (evnt) => {
+    this.setState({number: evnt.target.value})
+  }
+  
   addContact = (e) => {
     e.preventDefault();
-    const contact = {id: nanoid(), name: this.state.name}
-    // console.log(contact)
-    // this.setState(prevState => prevState.contacts = [...prevState.contacts, contact])
-    this.setState(state => state = {name: '' , contacts: [...state.contacts, contact]})
-    console.dir(e.target.elements)
+    const contact = {id: nanoid(), name: this.state.name, number: this.state.number}
+    this.setState(state => state = {contacts: [...state.contacts, contact], name: '', number: ''})
     e.target.reset()
-    // console.log(this.state.name)
-    // reset()
+  }
+
+  search = (evnt) => {
+    const textSearch = evnt.target.value
+    const filterContacts = this.state.contacts.filter({name} => )
+    this.setState({filter: filterContacts})
+
   }
 
   render () {
@@ -32,31 +45,53 @@ export class App extends Component {
       <div
         style={{
           height: '100vh',
-          display: 'flex',
+          // display: 'flex',
           justifyContent: 'center',
           alignItems: 'center',
           fontSize: 40,
           color: '#010101'
         }}
       >
-        <h2>Phonebook</h2>
+        <h2>Phonebook</h2> 
         <form action="" onSubmit = {(evnt) => this.addContact(evnt)}>
-          <input
-          onChange = {(e) => this.recordName(e)}
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          />
+          <label>
+            Name
+            <input
+            onChange = {(e) => this.recordName(e)}
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            />
+          </label>
+          <label htmlFor="">
+            Number
+            <input
+              onChange = {(e) => this.recordNumber(e)}
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          
           <button type="submit" >
             Add contact
           </button>
         </form>
         <h3>Contact</h3>
+        <label htmlFor="">
+          Find contacts by name
+          <input
+            onChange={(e)=> this.search(e)}
+            type="text" 
+          />
+        </label>
         <ul>
           {contacts.map(contact => 
-            <li key = {contact.id}>{contact.name}</li>
+            <li key = {contact.id}>{contact.name}: {contact.number}</li>
           )}
         </ul>
       </div>
@@ -64,6 +99,4 @@ export class App extends Component {
   }
 }
 
-// export const App = () => {
-  
-// };
+console.dir(App)
